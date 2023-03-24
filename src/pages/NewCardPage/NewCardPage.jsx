f8b2c4c50c3968b0405b317adecd01e065680516
EdASalazar <edasalazar123@gmail.com>
2023-03-24 15:44:52 -0700

    Setting up new card form

--- src/pages/NewCardPage/NewCardPage.jsx
+++ src/pages/NewCardPage/NewCardPage.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+
 export default function NewCardPage() {
+  const [newCard, setNewCard] =useState({});
+
+  function handleChange(evt) {
+    setNewCard({...newCard, [evt.target.name]: evt.targe.value})
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    // Baby steps
+    console.log(newCard);
+    alert({newCard})
+  }
+
   return (
     <div className="NewCardPage">
       <h4>Add A Card</h4>
-      <form action="submit">
-        <input type="text" name="" id="" />
+      <form action="submit" className="NewCardForm" onSubmit={handleSubmit}>
+        <input type="text" 
+        name="sideOneWord" 
+        value={newCard.text}
+        placeholder="Side One Phrase..."
+        onChange={handleChange}
+        />
+        <button type="submit">Add A Card</button>
       </form>
     </div>
   );
