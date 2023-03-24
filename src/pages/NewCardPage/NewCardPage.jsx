9870b87fac1b5de10d948ced36e1f4356bc3625e
EdASalazar <edasalazar123@gmail.com>
2023-03-24 15:12:42 -0700

    adding form

--- src/pages/NewCardPage/NewCardPage.jsx
+++ src/pages/NewCardPage/NewCardPage.jsx
@@ -3,7 +3,7 @@ export default function NewCardPage() {
     <div className="NewCardPage">
       <h4>Add A Card</h4>
       <form action="submit">
-        
+        <input type="text" name="" id="" />
       </form>
     </div>
   );
