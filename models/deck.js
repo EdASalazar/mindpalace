493027341e9656644eb9115564d27b38c3b03cc7
EdASalazar <edasalazar123@gmail.com>
2023-03-24 14:22:16 -0700

    Finishing up the deck upject

--- models/deck.js
+++ models/deck.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const deckSchema = new Schema({
-  name: String,
-  
+// Trying to think ahead about what I might need down the road
+// Debating about how I want to share decks. 
 
-});
+const deckSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  }, 
+  catagory: String,
+  subject: String,
+  lastUsed: Date,
+  timesUsed: Number,
+   card: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Card',
+    required: true,
+   }],
+   user:{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+   }
+}, {
+  timestamps: true
+}
+);
 
 module.export = mongoose.model('Deck', deckSchema)
