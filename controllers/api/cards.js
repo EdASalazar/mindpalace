303cebadbe47a6fc42cfbab07e4cbaf6e90e7599
EdASalazar <edasalazar123@gmail.com>
2023-03-24 17:41:17 -0700

    Failing to create card

--- controllers/api/cards.js
+++ controllers/api/cards.js
@@ -1,5 +1,5 @@
 const User = require('../../models/user');
-const Card = require('../../models/note')
+const Card = require('../../models/note');
 
 module.exports = {
   create,
@@ -9,5 +9,9 @@ async function create(req, res){
   try {
     req.body.user = req.user._id;
     const card = await Card.create(req.body)
+    res.json(card);
+    console.log(card)
+  } catch (err) {
+    res.status(400).json(err)
   }
 }
