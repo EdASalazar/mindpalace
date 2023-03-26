8f3740738e639615f78c57e7b1facb640f881cbe
EdASalazar <edasalazar123@gmail.com>
2023-03-26 00:25:57 -0700

    End of day

--- src/components/DeckCardList/DeckCardList.jsx
+++ src/components/DeckCardList/DeckCardList.jsx
@@ -1,9 +1,13 @@
-export default function DeckCardList({sideOneWord, sideTwoWord }) {
+export default function DeckCardList({sideOneWord, sideTwoWord, deckName, deckId  }) {
   return (
    <div>
      <h4>Deck Card List</h4>
+       <p>{deckName}</p>
        <p>{sideOneWord}</p>
        <p>{sideTwoWord}</p>
+       <p>{deckId}</p>
+
+
    </div> 
 
     
--- src/components/DeckDetail/DeckDetail.jsx
+++ src/components/DeckDetail/DeckDetail.jsx
@@ -2,17 +2,20 @@ import { set } from "mongoose";
 import DeckCardList from "../DeckCardList/DeckCardList";
 
 export default function DeckDetail({ setActiveDeck, deckId, deckName, cardsForDeck }) {
+  
    const CardsInDeck = cardsForDeck.map((card, idx) => 
    <DeckCardList 
     sideOneWord={card.sideOneWord}
     sideTwoWord={card.sideTwoWord}
     key={idx}
+    deckName={deckName}
+    deckId={deckId}
    />
    )
   
   return (
     <div className="DeckDetailPage" onClick={() => setActiveDeck(deckId)}>
-      <h4>{deckName}</h4>
+      {/* <h4>{deckName}</h4> */}
       <p>{deckId} </p>
       {CardsInDeck}
     </div>
