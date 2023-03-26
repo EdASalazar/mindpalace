abf566994578ca00a958b443e96d3c4cf7eeaab3
EdASalazar <edasalazar123@gmail.com>
2023-03-25 19:33:00 -0700

    Changing the state of the deck list detail

--- src/components/DeckDetail/DeckDetail.jsx
+++ src/components/DeckDetail/DeckDetail.jsx
@@ -1,12 +1,12 @@
-import FlashCard from "../FlashCard/FlashCard";
+import { set } from "mongoose";
 
 export default function DeckDetailPage({ setActiveDeck, deckId, deckName, }) {
   
   
   return (
-    <>
+    <div className="DeckDetailPage" onClick={()=>setActiveDeck(deckId)}>
       <h4>{deckName}</h4>
       <p>{deckId}</p>
-    </>
+    </div>
   );
 }
