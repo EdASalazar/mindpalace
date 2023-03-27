f0444d47683288e369f6c0f8299150a7019885d2
EdASalazar <edasalazar123@gmail.com>
2023-03-27 14:11:00 -0700

    Building out the deck details.

--- src/pages/DeckDetailPage/DeckDetailPage.jsx
+++ src/pages/DeckDetailPage/DeckDetailPage.jsx
@@ -3,15 +3,19 @@ import "./DeckDetailPage.css"
 
 export default function DeckDetailPage({ setActiveDeck, decks}) {
   const { deckId } = useParams();
-  console.log('deckId', deckId)
-  const deckDetail = decks.filter(deck => deck._id === deckId);
-  console.log('deckDetailFilter', deckDetail)
+  const test = '641e8c94687f2d707f755e14';
+
+  const deckDetail = decks.filter(deck => deck._id === test);
+  const deckArray = deckDetail[0];
+  console.log('deckId', deckId);
+  console.log('deckDetailFilter', deckDetail);
 
 
 
   return (
     <div className="DeckDetailPage">
-
+        <h1>{deckArray.name}</h1>
+        <p>Date Created: {new Date(deckArray.createdAt).toLocaleDateString()}</p>
     </div>
   );
 }
