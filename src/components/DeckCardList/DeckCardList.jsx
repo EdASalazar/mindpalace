c20bd478a272b3290bc2498676728cc4a0b5fe8a
EdASalazar <edasalazar123@gmail.com>
2023-03-27 14:15:43 -0700

    Working on fetching cards for deck id

--- src/components/DeckCardList/DeckCardList.jsx
+++ src/components/DeckCardList/DeckCardList.jsx
@@ -1,4 +1,7 @@
+
 export default function DeckCardList({sideOneWord, sideTwoWord, deckName, deckId  }) {
+  
+  
   return (
      <h1>Deck Card List</h1>
         // <li> {deckName}: {sideOneWord}:{sideTwoWord}</li>
--- src/pages/DeckDetailPage/DeckDetailPage.jsx
+++ src/pages/DeckDetailPage/DeckDetailPage.jsx
@@ -3,13 +3,17 @@ import "./DeckDetailPage.css"
 
 export default function DeckDetailPage({ setActiveDeck, decks}) {
   const { deckId } = useParams();
+  // hard coding the user id until I figure out 
+  // what the issue is with useParams
   const test = '641e8c94687f2d707f755e14';
 
   const deckDetail = decks.filter(deck => deck._id === test);
   const deckArray = deckDetail[0];
   console.log('deckId', deckId);
   console.log('deckDetailFilter', deckDetail);
-
+  // this will fetch the cards for the deck selected
+  // test will change the user id
+  setActiveDeck(test)
 
 
   return (
