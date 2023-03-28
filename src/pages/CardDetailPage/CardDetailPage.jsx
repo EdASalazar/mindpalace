3f06eee9e45fd0ee72301fc0650d7cab33b20a7e
EdASalazar <edasalazar123@gmail.com>
2023-03-28 08:58:58 -0700

    Starting the get card process and styling some things

--- src/pages/CardDetailPage/CardDetailPage.jsx
+++ src/pages/CardDetailPage/CardDetailPage.jsx
@@ -1,7 +1,24 @@
 import { useParams } from  "react-router-dom"
+import { useState, useEffect } from "react";
+import * as cardsAPI from '../../utilities/cards-api'
 
 export default function CardDetailPage() {
-  const { cardId } = useParams();
+  // const { cardId } = useParams();
+  // const [cardDetail, setCardDetail] = useState(null);
+
+  // useEffect(function() {
+  //   if (!cardId) {
+  //     console.log("No card chosen")
+  //   } else {
+  //   async function getCards() {
+  //     const card = await cardsAPI.getCard(cardId);
+  //     setCardDetail(card);
+  //   }
+  //   getCards();
+  //   }
+  // }, []);
+  
+
   return (
     <p>Card Detail Page</p>
   )
