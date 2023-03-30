15f62dcd869290d0ac76dc2bb7d8b76dcfa2a7a4
EdASalazar <edasalazar123@gmail.com>
2023-03-30 14:18:30 -0700

    Refactored the deck list and detail page

--- src/pages/DeckDetailPage/DeckDetailPage.jsx
+++ src/pages/DeckDetailPage/DeckDetailPage.jsx
@@ -1,28 +1,14 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from 'react';
 import "./DeckDetailPage.css"
-import * as decksAPI from '../../utilities/decks-api'
 import DeckCardList from "../../components/DeckCardList/DeckCardList";
 
 export default function DeckDetailPage({ decks }) {
-  const [cardsForDeck, setCardsForDeck] = useState([]);
   const { deckId } = useParams();
 
   const deckDetail = decks.filter(deck => deck._id === deckId);
   const deckArray = deckDetail[0];
+  const deckCards = deckArray.cards;
 
-  useEffect(function() {
-    if (!deckId) {
-      console.log("No deck chosen")
-    } else {
-    async function getCards() {
-      const cards = await decksAPI.getAllCardsForDeck(deckId);
-      setCardsForDeck(cards);
-    }
-    getCards();
-    }
-  }, []);
-  
   return (
     <div className="DeckDetailPage">
       <div className="DeckDetails">
@@ -33,7 +19,7 @@ export default function DeckDetailPage({ decks }) {
       </div>
       <aside>
       <h2>Phrases:</h2>
-         <DeckCardList cardsForDeck={cardsForDeck}/>
+         <DeckCardList cardsForDeck={deckCards}/>
       </aside>
     </div>
   );
