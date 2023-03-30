c0bee91435a9806dc9d09b745ac781d636b64eac
EdASalazar <edasalazar123@gmail.com>
2023-03-30 10:12:18 -0700

    Adding notifications to wrong and right

--- src/components/BoardCardItem/BoardCardItem.jsx
+++ src/components/BoardCardItem/BoardCardItem.jsx
@@ -2,31 +2,39 @@ import { useEffect, useState } from "react";
 
 import "./BoardCardItem.css"
 
-export default function BoardCardItem({ 
-  sideOne, sideTwo, setI, setJ, setAnswer,
+export default function BoardCardItem({ sideOne, sideTwo, setI, setJ, setAnswer,
   i, j, answer, length 
 }) {
-  
   const[visible, setVisible] = useState(false);
+
+  async function sleep(seconds) {
+    return new Promise((resolve) =>setTimeout(resolve, seconds * 1000));
+    }
+    
+    
 
 
   function handleChange(evt) {
     setAnswer({...answer, [evt.target.name]: evt.target.value})
   };
 
-   function handleSubmit(evt) {
+function handleSubmit(evt) {
     evt.preventDefault();
     console.log('submitted');
+    async function checkWin() {
     if (answer.text === `${sideTwo}`) {
-      alert('correct')
+      setVisible(true)
+      await sleep(1)
+      setVisible(false)
       setI(i + 1);
       setJ(j + 1);
-      setVisible(true)
       setAnswer("");
     } else {
       alert('try again');
     }
   }
+  checkWin();
+}
 
   useEffect(function() {
     if(j === length) {
@@ -35,17 +43,16 @@ export default function BoardCardItem({
     }
   }, [j]);
 
-
-  
-
-
   return(
     <div className="FlashCard">
       <div className="FlashCardSideOne">
         {sideOne}
       </div>
+      <div className="winCheckResponse">
+        {visible && <h4>Correct!</h4>}
+      </div>
       <div className="FlashCardSideTwo">
-       {visible && {sideTwo}}
+       {visible && <div>{sideTwo}</div>}
       </div>
       <div className="AnswerForm">
         <form action="">
