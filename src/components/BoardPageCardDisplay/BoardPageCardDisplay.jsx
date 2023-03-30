8a8589d881cb4f4d70af928afe6ba76dee5e67b7
EdASalazar <edasalazar123@gmail.com>
2023-03-29 21:19:22 -0700

    Starting refactoring. Hope nothing breaks

--- src/components/BoardPageCardDisplay/BoardPageCardDisplay.jsx
+++ src/components/BoardPageCardDisplay/BoardPageCardDisplay.jsx
@@ -3,14 +3,22 @@ import BoardCardItem from "../BoardCardItem/BoardCardItem";
 import "./BoardPageCardDisplay.css"
 
 export default function BoardPageCardDisplay({ cardsForBoard }) {
-
-  const cardInfo = cardsForBoard.slice(2,3).map((card) => 
+  const [i, setI] = useState(0);
+  const [j, setJ] = useState(1)
+  
+  
+  const cardInfo = cardsForBoard.slice(i,j).map((card) => 
   <BoardCardItem 
   sideOne={card.sideOneWord}
   sideTwo={card.sideTwoWord}
   card={card.card}
   key={card._id} 
   />);
+
+  
+
+
+
 
     
   return (
