b81af9d268832eed218906ecc69729d6546def7c
EdASalazar <edasalazar123@gmail.com>
2023-03-29 21:13:57 -0700

    added a slice to the map

--- src/components/BoardPageCardDisplay/BoardPageCardDisplay.jsx
+++ src/components/BoardPageCardDisplay/BoardPageCardDisplay.jsx
@@ -4,16 +4,14 @@ import "./BoardPageCardDisplay.css"
 
 export default function BoardPageCardDisplay({ cardsForBoard }) {
 
-  const cardInfo = cardsForBoard.map((card) => 
+  const cardInfo = cardsForBoard.slice(2,3).map((card) => 
   <BoardCardItem 
   sideOne={card.sideOneWord}
   sideTwo={card.sideTwoWord}
   card={card.card}
   key={card._id} 
   />);
- console.log("2", cardsForBoard)
- const [c0, c1, ...rest] = cardsForBoard;
- console.log("c2", c2)
+
     
   return (
     <div className="BoardPageCardDisplay">
